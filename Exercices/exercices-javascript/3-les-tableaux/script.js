1464d0ea90cd93f05dfb5ffbbbc8dde28c73fb78
Julie Vanderbyse <vanderbysej@possibles.org>
2020-06-21 11:25:49 +0100

    exercices-js-s3

---
+++ Exercices/exercices-javascript/3-les-tableaux/script.js
@@ -0,0 +1,23 @@
+// Exercice 1: ajoute "Ted" au tableau suivant
+
+let castArray= ["Robin", "Barney", "Lily", "Marshal"];
+
+// Exercice 2: Affiche l'index de "Ted" dans le tableau dans la console
+
+// Exercice 3: Affiche l'index de Robin dans le tableau dans la console
+
+// Exercice 4: retire "Robin" du tableau
+
+// Exercice 5: retire "Ted" du tableau
+
+// Exercice 6: ajoute "Ted" et "Robin" au tableau
+
+// Exercice 7: Affiche le nouvel index de Ted et Robin dans le tableau dans la console
+
+// Exercice 8: à l'aide d'une boucle, affiche chaque élément du tableau dans la console
+
+// Exercice 9: à l'aide de la méthode .findIndex(), trouve l'index de Lily, ensuite, affiche Lily dans la console
+
+// Exercice 10: à l'aide d'une boucle, multiplie par 2 chaque élément du tableau suivant, crée un nouveau tableau à l'aide de la méthode .push(), affiche le nouveau tableau dans la console
+
+let multiplicationArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
